23e932c04db359dfaf45a98c2bf3bcb7aeec4a75
holbiwan <sabrinapapeau@gmail.com>
2026-01-23 16:10:39 +0100

    Initial commit - Villa Bliss
    Transformation du projet House of Bliss eShop en Villa Bliss.
    Villa Bliss est désormais une plateforme de conseil en bien-être, aromathérapie et coaching.

---
+++ README.md
@@ -0,0 +1,89 @@
+<p align="center">
+  <img src="https://zupimages.net/up/24/06/1e99.png" alt="Villa Bliss Logo"/>
+  <h1 align="center">Villa Bliss</h1>
+  <p align="center">Votre havre de paix pour le bien-\u00eatre</p>
+  <p align="center">
+    <b>:star: Bienvenue \u00e0 Villa Bliss :star:</b>
+  </p>
+</p>
+
+### Link to my landing page : https://holbiwan.github.io/Holbiwan-HOB.github.io/
+
+Votre destination sereine pour des conseils en bien-\u00eatre, aromath\u00e9rapie et coaching.
+Cette plateforme est sp\u00e9cialement con\u00e7ue pour vous accompagner dans votre qu\u00eate
+d'\u00e9quilibre et de s\u00e9r\u00e9nit\u00e9.
+
+# 📋 Content
+
+1. 🔨 Installation - Setting Up
+2. 🛠️ Configuration
+3. 💽 Database
+4. 🔒 Payments and Security
+5. 📊 Tools Usage
+6. 🏃 Running the Application
+7. ©️ License
+   
+## 🔨1. Installation - Setting Up
+
+Avant de plonger dans l'exp\u00e9rience Villa Bliss, assurez-vous de r\u00e9pondre aux exigences suivantes :
+
+- Node.js 12.x or higher  
+- Python 3.8 or higher  
+- Pip for Python dependencies  
+
+**Setting Up:**  
+
+Clone this repository to your local machine to set up the platform:
+
+```
+git clone https://github.com/Holbiwan/Villa-Bliss.git
+cd Villa-Bliss
+```
+
+
+```
+npm install
+pip install -r requirements.txt
+```
+
+This section is dedicated to initial setup procedures.
+
+## 🛠️ 2. Configuration
+
+Configure your application by editing the `.env` file to include your specific environment variables and secrets.
+
+## 💽 3. Database
+
+La plateforme utilise SQLite pour une gestion efficace des donn\u00e9es. Assurez-vous que votre base de donn\u00e9es est configur\u00e9e correctement.
+
+## 🔒 4. Payments and Security
+
+Pour des transactions s\u00e9curis\u00e9es (si n\u00e9cessaire \u00e0 l'avenir), nous int\u00e9grons PayPal.
+
+## 📊 5. Tools Usage
+
+Cette section pr\u00e9sente un aper\u00e7u des outils utilis\u00e9s pour la plateforme Villa Bliss.
+
+<div align="center">
+    <img src="https://skillicons.dev/icons?i=python,html,css,js,react,nodejs,django,sqlite,bootstrap" /><br>
+</div>
+
+
+## 🏃 6. Running the Application
+
+```Start the development server with:
+npm start
+```
+
+```For the Django backend:
+python3 manage.py runserver
+```
+Adding Products:
+
+Pour ajouter du contenu \u00e0 votre site, naviguez vers le panneau d'administration \u00e0 `/admin` et utilisez l'interface intuitive pour g\u00e9rer votre contenu.
+
+## ©️ 7. License
+
+This project is licensed under the MIT License
+
+**Author:** Sabrina Papeau - [Github](https://github.com/Holbiwan)
---
+++ frontend/settings.py
@@ -0,0 +1,172 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("The DJANGO_SECRET_KEY environment variable is not set")
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django_countries',
+    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'dj_rest_auth',
+    'core',
+]
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    
+]
+
+ROOT_URLCONF = 'frontend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            
+        },
+    },
+]
+
+WSGI_APPLICATION = 'frontend.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Auth
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
+
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+
+# for the connection from react app
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+# permission classes
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' :(
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES' :(
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
